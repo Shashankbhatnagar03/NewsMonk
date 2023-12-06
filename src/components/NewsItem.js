6f706f4fd1d431cb0e3328e2cab9bf2e81ae4ae7
Shashankbhatnagar03 <Shashank.bhatnagar3@gmail.com>
2023-12-06 20:26:43 +0530

    Initial commit

---
+++ src/components/NewsItem.js
@@ -0,0 +1,49 @@
+import React from "react";
+
+const NewsItem =(props)=> {
+  
+    let { title, description, imageUrl, newsUrl, author, date, source } =
+      props;
+    return (
+      <div className="my-3">
+        <div className="card">
+          <span
+            className="position-absolute top-0 translate-middle badge rounded-pill bg-danger"
+            style={{ left: "90%", zIndex: 1 }}
+          >
+            {source}
+          </span>
+          <img
+            src={
+              !imageUrl
+                ? "https://media.istockphoto.com/id/1182477852/photo/breaking-news-world-news-with-map-backgorund.jpg?s=1024x1024&w=is&k=20&c=S9FBe3KUvooZHZktJzr8Nt94wtg56BQTQiqAz8zUK8M="
+                : imageUrl
+            }
+            className="card-img-top"
+            alt="..."
+          />
+          <div className="card-body">
+            <h5 className="card-title">{title}</h5>
+            <p className="card-text">{description}...</p>
+            <p className="card-text">
+              <small className="text-muted">
+                By {!author ? "anonymous" : author} on{" "}
+                {new Date(date).toGMTString()}
+              </small>
+            </p>
+            <a
+              rel="noreferrer"
+              href={newsUrl}
+              target="_blank"
+              className="btn bt-sm btn-dark"
+            >
+              Read More
+            </a>
+          </div>
+        </div>
+      </div>
+    );
+  
+}
+
+export default NewsItem;
